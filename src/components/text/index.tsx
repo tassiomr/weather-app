b0edaeb07575611fa6757d288af5f643cc0dc221
Tassio Rocha <tassiomarcosr@outlook.com>
2022-02-04 11:50:58 -0300

    Update text component

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import * as React from 'react';
+import { TextType } from '../../typescript/type';
 import { Component } from './styles';
 
 type Props = {
   text: string;
+  textType: TextType
 };
 
-export const Text: React.FC<Props> = ({ text }) => {
-  return <Component>{text}</Component>;
+export const Text: React.FC<Props> = ({ text, textType }) => {
+  return <Component textType={textType}>{text}</Component>;
 };

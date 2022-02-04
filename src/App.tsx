dee925beac7e03a6e4f115ffdb43d4584d7ce2fd
Tassio Rocha <tassiomarcosr@outlook.com>
2022-02-04 08:26:33 -0300

    Add StatusBarColor

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
-import { useColorScheme } from 'react-native';
+import { StatusBar, useColorScheme } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import sizes from './configs/sizes';
 import Themes from './configs/theme';
 import { Home } from './pages';
 import { GeoLocationProvider } from './context/geolocation.context';
+import { palette } from './configs/palette';
 
 const App = () => {
   const theme = useColorScheme();
   return (
     <ThemeProvider theme={{ colors: Themes[theme || 'light'], sizes }}>
+      <StatusBar backgroundColor={palette.primary} />
       <GeoLocationProvider>
         <Home />
       </GeoLocationProvider>

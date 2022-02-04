1b56049a3c273dd733309232ec3b315f89c3d322
Tassio Rocha <tassiomarcosr@outlook.com>
2022-02-04 18:09:28 -0300

    Call correct way of home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,35 +1,16 @@
 import * as React from 'react';
-import { Image } from 'react-native';
-import { Body, Spacer, Te Icon } from '../../components';
-import { GetCurrentDay, GetCurrentHour } from '../../tools';
-import { HourContainer, WeatherContainer, CountryWrapper, WeatherWrapper, TextWeatherWrapper } from './styles';
-
+import { Body } from '../components';
+import { useGeoLocation } from '../context/geolocation.context';
+import { useWeather } from '../context/weather.context';
+import { ErrorFragment, WeatherFragment } from '../fragments';
 
 export const Home: React.FC = () => {
-  return (
-    <Body isLoading={false}>
-        <HourContainer>
-          <Text text={GetCurrentHour()} textType="header" />
-          <Text text={GetCurrentDay()} textType="paragraph" />
-        </HourContainer>
-        <WeatherContainer>
-          <CountryWrapper>
-            <Icon name='map-marker' size='medium' color='blue' />
-            <Text textType='paragraph' text={'Istambul - TK'} />
-          </CountryWrapper>
-          <Spacer size='large' />
-          <WeatherWrapper>
-            <TextWeatherWrapper>
-              <Text textType='subtitle' text={'32°'} />
-              <Text textType='paragraph' text={'Sunny'} />
-              <Text textType='paragraph' text={'Humity: 30%'} />
-              <Text textType='paragraph' text={'Feels like: 32°'} />
-              <Text textType='paragraph' text={'Today Max Temp: 32°'} />
-              <Text textType='paragraph' text={'Today Min Temp: 16°'} />
-            </TextWeatherWrapper>
-            <Image source={{ uri: 'https://openweathermap.org/img/wn/10d@4x.png' }} style={{ width: 150, height: 150 }} />
-          </WeatherWrapper>
-        </WeatherContainer>
-    </Body>
-  );
+  const { weather } = useWeather();
+  const { getGeoLocation } = useGeoLocation();
+
+  return <Body isLoading={false} onPress={getGeoLocation}>
+    {
+      !!weather ? <WeatherFragment weather={weather} /> : <ErrorFragment />
+    }
+  </Body>
 };

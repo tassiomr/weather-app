cb28edd65ff4641fbc5c2aad7bda3be516869a4a
Tassio Rocha <tassiomarcosr@outlook.com>
2022-02-04 11:56:52 -0300

    Create constants file

---
+++ src/configs/constants.ts
@@ -0,0 +1,5 @@
+export const constants = { 
+    months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    days: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+}

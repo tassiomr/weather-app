a01200e2580f32de56275138585367dcfbd7f549
Tassio Rocha <tassiomarcosr@outlook.com>
2022-02-04 23:19:26 -0300

    Create components test

---
+++ __tests__/ui/components/body.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { render } from '@testing-library/react-native'
+import { View } from 'react-native'
+import { Body } from '../../../src/components';
+
+describe('Testing body component', () => {
+    it('should isLoading is true show ActivityIndicator', () => {
+        const container = render(<Body isLoading={true}><View/></Body>);
+        expect(container.toJSON()).toMatchSnapshot();
+    })
+
+    it('should isLoading is false show children', () => {
+        const container = render(<Body isLoading={false}><View/></Body>);
+        expect(container.toJSON()).toMatchSnapshot();
+    })
+})
---
+++ __tests__/ui/components/icon.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { render } from '@testing-library/react-native'
+import { View } from 'react-native'
+import { Icon } from '../../../src/components';
+
+describe('Testing icon component', () => {
+    it('should color is null', () => {
+        const container = render(<Icon size="extra-large" name="camera" />);
+        expect(container.toJSON()).toMatchSnapshot();
+    })
+
+    it('should color is not null', () => {
+        const container = render(<Icon size="extra-large" name="camera" color='blue' />);
+        expect(container.toJSON()).toMatchSnapshot();
+    })
+})

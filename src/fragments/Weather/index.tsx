08f79ad707f5421fa724b3568350f6fbdbb51ba4
Tassio Rocha <tassiomarcosr@outlook.com>
2022-02-06 14:39:56 -0300

    Update Weather

--- src/fragments/Weather/index.tsx
+++ src/fragments/Weather/index.tsx
@@ -2,14 +2,13 @@ import * as React from 'react';
 import { Image } from 'react-native';
 import { Spacer, Text, Icon } from '../../components';
 import { constants } from '../../configs/constants';
-import { GetCurrentDay, GetCurrentHour } from '../../tools';
 import { Weather } from '../../typescript/type';
 import {
-  HourContainer,
   WeatherContainer,
   CountryWrapper,
   WeatherWrapper,
   TextWeatherWrapper,
+  Unavailable,
 } from './styles';
 
 type Props = {
@@ -18,68 +17,69 @@ type Props = {
 
 export const WeatherFragment: React.FC<Props> = ({ weather }) => (
   <>
-    <HourContainer testID={constants.testsId.weatherFragment}>
-      <Text
-        testID={constants.testsId.weatherHourText}
-        text={GetCurrentHour()}
-        textType="header"
-      />
-      <Text
-        testID={constants.testsId.weatherDayText}
-        text={GetCurrentDay()}
-        textType="paragraph"
-      />
-    </HourContainer>
     <WeatherContainer>
-      <CountryWrapper>
-        <Icon name="map-marker" size="medium" color="blue" />
-        <Text
-          testID={constants.testsId.weatherCountryText}
-          textType="paragraph"
-          text={`${weather.name} - ${weather.sys.country}`}
-        />
-      </CountryWrapper>
-      <Spacer size="large" />
-      <WeatherWrapper>
-        <TextWeatherWrapper>
+      {weather ? (
+        <>
+          <CountryWrapper>
+            <Icon name="map-marker" size="medium" color="blue" />
+            <Text
+              testID={constants.testsId.weatherCountryText}
+              textType="paragraph"
+              text={`${weather.name} - ${weather.sys.country}`}
+            />
+          </CountryWrapper>
+          <Spacer size="large" />
+          <WeatherWrapper>
+            <TextWeatherWrapper>
+              <Text
+                testID={constants.testsId.weatherTempText}
+                textType="subtitle"
+                text={`${weather.main.temp.toFixed(0)}°`}
+              />
+              <Text
+                testID={constants.testsId.weatherText}
+                textType="paragraph"
+                text={weather.weather[0].main}
+              />
+              <Text
+                testID={constants.testsId.weatherHumityText}
+                textType="paragraph"
+                text={`Humidity: ${weather.main.humidity}%`}
+              />
+              <Text
+                testID={constants.testsId.weatherFeelsText}
+                textType="paragraph"
+                text={`Feels like: ${weather.main.feels_like.toFixed(0)}°`}
+              />
+              <Text
+                testID={constants.testsId.weatherMaxTempText}
+                textType="paragraph"
+                text={`Today Max Temp: ${weather.main.temp_max.toFixed(0)}°`}
+              />
+              <Text
+                testID={constants.testsId.weatherMinTempText}
+                textType="paragraph"
+                text={`Today Min Temp: ${weather.main.temp_min.toFixed(0)}°`}
+              />
+            </TextWeatherWrapper>
+            <Image
+              source={{
+                uri: `https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`,
+              }}
+              style={{ width: 150, height: 150 }}
+            />
+          </WeatherWrapper>
+        </>
+      ) : (
+        <Unavailable>
           <Text
-            testID={constants.testsId.weatherTempText}
-            textType="subtitle"
-            text={`${weather.main.temp.toFixed(0)}°`}
-          />
-          <Text
-            testID={constants.testsId.weatherText}
             textType="paragraph"
-            text={weather.weather[0].main}
+            textAlign="center"
+            testID="no-permission-text"
+            text="Service unavailable!"
           />
-          <Text
-            testID={constants.testsId.weatherHumityText}
-            textType="paragraph"
-            text={`Humidity: ${weather.main.humidity}%`}
-          />
-          <Text
-            testID={constants.testsId.weatherFeelsText}
-            textType="paragraph"
-            text={`Feels like: ${weather.main.feels_like.toFixed(0)}°`}
-          />
-          <Text
-            testID={constants.testsId.weatherMaxTempText}
-            textType="paragraph"
-            text={`Today Max Temp: ${weather.main.temp_max.toFixed(0)}°`}
-          />
-          <Text
-            testID={constants.testsId.weatherMinTempText}
-            textType="paragraph"
-            text={`Today Min Temp: ${weather.main.temp_min.toFixed(0)}°`}
-          />
-        </TextWeatherWrapper>
-        <Image
-          source={{
-            uri: `https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`,
-          }}
-          style={{ width: 150, height: 150 }}
-        />
-      </WeatherWrapper>
+        </Unavailable>
+      )}
     </WeatherContainer>
   </>
 );
